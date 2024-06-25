50537c350052c27ced7665826770578d79cfdaab
somyakant9 <msomya143@gmail.com>
2024-06-25 22:14:01 +0530

    added hamburger and downloadable resume

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -2,7 +2,34 @@ import React from "react";
 import styles from "./Welcome.module.css";
 import { Link } from "react-scroll";
 import homepage from "../images/giphy4.gif";
+import menuIcon from "../images/app-menu-icon-sign-symbol-design-free-png.webp";
+
 const Welcome = () => {
+  const handleDownload = (event) => {
+    event.preventDefault(); // Prevent the default anchor click behavior
+
+    // Open the document in a new tab
+    const newTab = window.open(event.currentTarget.href, "_blank");
+    if (newTab) {
+      newTab.focus();
+    }
+
+    // Trigger the download
+    const downloadLink = document.createElement("a");
+    downloadLink.href =
+      "https://drive.google.com/uc?export=download&id=1EstWmBqMIp1BXl6i4g82FHbkDQKbtVjZ";
+    downloadLink.download = "";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
+
+  const openMenu= (e) => {
+   
+    console.log(e);
+     
+  }
+
   return (
     <>
       <div
@@ -14,32 +41,47 @@ const Welcome = () => {
           backgroundSize: "cover",
           backgroundRepeat: "no-repeat",
           backgroundAttachment: "fixed",
-          backgroundPosition:"center center",
-      
+          backgroundPosition: "center center",
         }}
       >
-       
-        <div className={styles.navbar}>
-          <Link to="about" spy={true} smooth={true}>
-            <div className={styles.link}>About</div>
-          </Link>
-          <Link to="projects" spy={true} smooth={true}>
-            <div className={styles.link}>Projects</div>
-          </Link>
-          <Link to="skills" spy={true} smooth={true}>
-            <div className={styles.link}>Skills</div>
-          </Link>
-          <Link to="contacts" spy={true} smooth={true}>
-            <div className={styles.link}>Contact</div>
-          </Link>
-          <a
-            className={styles.resume}
-            href="https://drive.google.com/file/d/1EstWmBqMIp1BXl6i4g82FHbkDQKbtVjZ/view?usp=sharing"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            <div className={styles.link}>Resume</div>
-          </a>
+        <div>
+          <div className={styles.navbar} name="navbar">
+            <Link to="about" spy={true} smooth={true} className={styles.links}>
+              About
+            </Link>
+            <Link
+              to="projects"
+              spy={true}
+              smooth={true}
+              className={styles.links}
+            >
+              Projects
+            </Link>
+            <Link to="skills" spy={true} smooth={true} className={styles.links}>
+              Skills
+            </Link>
+            <Link
+              to="contacts"
+              spy={true}
+              smooth={true}
+              className={styles.links}
+            >
+              Contact
+            </Link>
+            <a
+              className={styles.resume}
+              href="https://drive.google.com/file/d/1EstWmBqMIp1BXl6i4g82FHbkDQKbtVjZ/view?usp=sharing"
+              target="_blank"
+              rel="noreferrer noopener"
+              onClick={handleDownload}
+            >
+              Resume
+            </a>
+            {/* https://drive.google.com/uc?export=download&id=1EstWmBqMIp1BXl6i4g82FHbkDQKbtVjZ */}
+          </div>
+          <div className={styles.menu} onClick={openMenu}>
+            <img src={menuIcon} alt="menu" className={styles.icon} />
+          </div>
         </div>
         <div className={styles.nameDiv}>
           <p className={styles.text1}>I am Somyakant Mohapatra</p>
@@ -47,7 +89,6 @@ const Welcome = () => {
           <p className={styles.role}>FULL STACK WEB DEVELOPER</p>
         </div>
       </div>
-       
     </>
   );
 };
